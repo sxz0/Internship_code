cd2fedb8ca8147d5512e0be8a6dbd7e05b20e3c4
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-09-10 16:17:35 +0200

    Update launch_data_collection.sh

--- launch_data_collection.sh
+++ launch_data_collection.sh
@@ -5,5 +5,3 @@ do
 	echo "Execute './run_gpu_treasure.sh > feat_gpu_$ip &' in 192.168.0.$ip"
 	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore6; ./run_gpu_treasure.sh > feat_gpu_$ip &"
 done
-
-cat ./datasets/feat_gpu_* >> ./datasets/current_dataset.csv
