a3cce5ead92ac6c9829725003605c8b47625ac22
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-09-10 17:05:12 +0200

    Update TREASURE_tests_VC4.py

--- code/TREASURE_tests_VC4.py
+++ code/TREASURE_tests_VC4.py
@@ -4,14 +4,37 @@ import sys
 import socket,fcntl,struct
 from videocore.v3d import *
 from videocore.driver import Driver
+import random
+import hashlib
 
-s=int(sys.argv[1])
-def get_gpu_cycles(s):
+
+def sleep(duration, get_now=time.perf_counter):
+    now = time.time_ns()
+    duration=duration*1000000000
+    end = now + duration
+    while now < end:
+        now = time.time_ns()
+
+def get_QPU_freq(s):
     with RegisterMapping(Driver()) as regmap:
         with PerformanceCounter(regmap, [13,14,15,16,17,18,19]) as pctr:
             time.sleep(s)
             result = pctr.result()
             return sum(result)
+
+def cpu_true_random(n):
+     with RegisterMapping() as regmap:
+        with PerformanceCounter(regmap, [CORE_PCTR_CYCLE_COUNT]) as pctr:
+            a=os.urandom(n)
+            result = pctr.result()
+            return (sum(result))
+
+def cpu_hash():
+    with RegisterMapping() as regmap:
+        with PerformanceCounter(regmap, [13,14,15,16,17,28,19]) as pctr:
+            h=int(hashlib.sha256("test string".encode('utf-8')).hexdigest(), 16) % 10**8
+            result = pctr.result()
+            return (sum(result))
 
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,9 +42,10 @@ def getHwAddr(ifname):
     return ':'.join('%02x' % b for b in info[18:24])
 
 def main():
+	s=int(sys.argv[1])
 	results=[]
 	results.append(os.popen("vcgencmd measure_temp | cut -d = -f 2 | cut -d \"'\" -f 1").read()[:-1])
-	results.append(get_gpu_cycles(s))
+	results.append(get_QPU_freq(s))
 	
 	results.append(getHwAddr('eth0'))
 	print(*results, sep=',')
