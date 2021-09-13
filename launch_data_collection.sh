6d4c4056b909823b24d2a028b425c51773f1af42
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-09-13 13:14:20 +0200

    Add files via upload

--- launch_data_collection.sh
+++ launch_data_collection.sh
@@ -2,12 +2,12 @@
 
 for ip in $(seq 15 25)
 do
-	echo "Execute './run_gpu_treasure_VC6.sh > feat_gpu_$ip &' in 192.168.0.$ip"
-	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore6; ./run_gpu_treasure_VC6.sh > feat_gpu_$ip &"
+	echo "Execute './run_gpu_treasure_VC6.sh &' in 192.168.0.$ip"
+	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore6; ./run_gpu_treasure_VC6.sh > /dev/null &"
 done
 
-#for ip in $(seq 15 25)
-#do
-#	echo "Execute './run_gpu_treasure_VC4.sh > feat_gpu_$ip &' in 192.168.0.$ip"
-#	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore; ./run_gpu_treasure_VC4.sh > feat_gpu_$ip &"
-#done
+for ip in $(seq 4 14)
+do
+	echo "Execute './run_gpu_treasure_VC4.sh &' in 192.168.0.$ip"
+	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore; ./run_gpu_treasure_VC4.sh > /dev/null &"
+done
