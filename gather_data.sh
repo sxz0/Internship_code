41742e40eb138dc8ef96afa71160f6d84d28e786
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-09-13 13:14:38 +0200

    Add files via upload

--- gather_data.sh
+++ gather_data.sh
@@ -5,4 +5,9 @@ do
 	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore6/feat_gpu_* ./datasets/
 done
 
+for ip in $(seq 4 14)
+do
+	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore/feat_gpu_* ./datasets/
+done
+
 cat ./datasets/feat_gpu_* >> ./datasets/current_dataset.csv
