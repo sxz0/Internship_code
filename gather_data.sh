71f4d7b7a5e67a92d5df3647c53a11d42907bd23
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-09-14 13:00:16 +0200

    Update gather_data.sh

--- gather_data.sh
+++ gather_data.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Data from RPi4
-for ip in $(seq 15 25)
+for ip in $(seq 4 35)
 do
-	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore6/feat_gpu_* ./datasets/
+	echo "Gathering data from 192.168.0.$ip"
+	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/TREASURE/feat_gpu_* ./datasets/
 done
+
+# Data from RPi4
+#for ip in $(seq 15 25)
+#do
+#	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore6/feat_gpu_* ./datasets/
+#done
 
 
 # Data from RPi3
-for ip in $(seq 4 14)
-do
-	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore/feat_gpu_* ./datasets/
-done
+#for ip in $(seq 4 14)
+#do
+#	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore/feat_gpu_* ./datasets/
+#done
 
 
 # Data from RPi1
-for ip in $(seq 26 35)
-do
-	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore/feat_gpu_* ./datasets/
-done
+#for ip in $(seq 26 35)
+#do
+#	sshpass -p "raspberry" scp pi@192.168.0.$ip:/home/pi/py-videocore/feat_gpu_* ./datasets/
+#done
 
 
 #Concatenate data in temporal csv
