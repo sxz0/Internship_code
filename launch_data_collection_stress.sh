fe1c263f3d37696c2f7ec80f0a3e3b81ca352db0
Pedro Miguel <pedromiguel.sanchez@um.es>
2021-10-25 10:52:23 +0200

    Add files via upload

---
+++ launch_data_collection_stress.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+for ip in $(seq 4 35)
+do
+	echo "Execute './run_gpu_treasure.sh &' in 192.168.0.$ip"
+	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd TREASURE; ./run_gpu_treasure_stress.sh > /dev/null &"
+done
+
+for ip in $(seq 38 48)
+do
+	echo "Execute './run_gpu_treasure.sh &' in 192.168.0.$ip"
+	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd TREASURE; ./run_gpu_treasure_stress.sh > /dev/null &"
+done
+
+#for ip in $(seq 15 25)
+#do
+#	echo "Execute './run_gpu_treasure_VC6.sh &' in 192.168.0.$ip"
+#	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore6; ./run_gpu_treasure_VC6.sh > /dev/null &"
+#done
+#
+#for ip in $(seq 4 14)
+#do
+#	echo "Execute './run_gpu_treasure_VC4.sh &' in 192.168.0.$ip"
+#	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore; ./run_gpu_treasure_VC4.sh > /dev/null &"
+#done
+
+#for ip in $(seq 26 35)
+#do
+#	echo "Execute './run_gpu_treasure_VC4.sh &' in 192.168.0.$ip"
+#	sshpass -p "raspberry" ssh pi@192.168.0.$ip "cd py-videocore; ./run_gpu_treasure_VC4.sh > /dev/null &"
+#done
